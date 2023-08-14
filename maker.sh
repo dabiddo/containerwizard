c15ebffc271ff26875aba1b06d4c9f3a7d413a99
Dabiddo <dabiddo@gmail.com>
2023-08-13 22:37:48 -0700

    fixed devcontainer json for single docker and added comments to not get lost in code

--- maker.sh
+++ maker.sh
@@ -5,21 +5,23 @@ gum style --border normal --margin "1" --padding "1 2" --border-foreground 12 "H
 DIR=$(gum input --placeholder "What will be the name of the project?")
 
 #read dir
-
 if [[ ! -e $DIR ]]; then
     mkdir $DIR
 elif [[ ! -d $DIR ]]; then
     echo "$DIR already exists but is not a directory" 1>&2
 fi
+#export variable for future variable substitution
 export DIR
 echo -e "Working on it..."
 sleep 1; clear
 
 echo -e "Directory $(gum style --italic --foreground 99 $DIR) has been created.\n"
 
+#Start project configuration wizard
 echo "What kind of project?"
 CHOICE=$(gum choose --item.foreground 250 "Dockerfile" "Docker-Compose" "Devcontainer")
 
+#If user chooses to create a Docker file, ask white type of programming languange
 if [[ "$CHOICE" == "Dockerfile" ]]; then
     echo "So Dockferfile it is"
     touch "$DIR/Dockerfile"
@@ -44,24 +46,27 @@ if [[ "$CHOICE" == "Dockerfile" ]]; then
     paste ".stubs/Node.stub" "$DIR/Dockerfile" >"$DIR/Dockerfile"
     echo "Done!"
     fi
+#If user chooses a docker-compose: create docker-compose.yaml file, and ask for services to include
 elif [[ "$CHOICE" == "Docker-Compose" ]]; then
     echo "So Docker Compose it is"
-    touch "$DIR/docker-compose.yaml"
+    touch "$DIR/docker-compose.yaml" #WIP: ask for services to include in the yaml file
+#If user chooses to create a devcontainer project, create the hidden devcontainer directory, 
+# and ask what kind of project. [standalone, compose] to add the correct json config
 elif [[ "$CHOICE" == "Devcontainer" ]]; then
     echo "So Devcontiner it is"
     mkdir "$DIR/.devcontainer"
     CONTAINERCHOICE=$(gum choose "compose" "standalone")
     if [[ "$CONTAINERCHOICE" == "compose" ]]; then
-        touch "$DIR/.devcontainer/docker-compose.yaml"
+        touch "$DIR/.devcontainer/docker-compose.yaml" #for now empty: wip
         touch "$DIR/.devcontainer/devcontainer.json"
         envsubst < ".stubs/devcontainer/_compose.stub" > "$DIR/.devcontainer/devcontainer.json"
 
     elif [[ "$CONTAINERCHOICE" == "standalone" ]]; then
-        touch "$DIR/.devcontainer/Dockerfile"
-        paste ".stubs/devcontainer/_docker.stub" "$DIR/.devcontainer/devcontainer.json"
+        touch "$DIR/.devcontainer/Dockerfile" #for now empty: wip
+        envsubst < ".stubs/devcontainer/_docker.stub" > "$DIR/.devcontainer/devcontainer.json"
     fi
 fi
 
-#sleep 1; clear;
+sleep 1; clear;
 
 echo "Done!"
