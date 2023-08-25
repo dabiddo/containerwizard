2587b3d74005429612ade02e0fed12c5d794a57f
Dabiddo <dabiddo@gmail.com>
2023-08-24 19:03:43 -0700

    fixed docker route as paraemter, and read correct variable

--- maker.sh
+++ maker.sh
@@ -8,13 +8,19 @@ docker_language () {
     if [[ "$LCHOICE" == "PHP8.1" ]]; then
     echo "What $(gum style --italic --foreground 12 "Base") ?"
     PHPCHOICE=$(gum choose --item.foreground 250 "msft" "debian")
-
-    case PHPCHOICE in
+    
+    case $PHPCHOICE in
     "debian")
+    echo "debian selected"
     paste ".stubs/php/php81_debian.stub" "$1/Dockerfile" >"$1/Dockerfile"
     ;;
-    *)
+    "msft")
+    echo "msft selected"
     paste ".stubs/php/php81_msft.stub" "$1/Dockerfile" >"$1/Dockerfile"
+    ;;
+    *)
+    echo "none"
+    #paste ".stubs/php/php81_msft.stub" "$1/Dockerfile" >"$1/Dockerfile"
     ;;
     esac
     
@@ -26,7 +32,7 @@ docker_language () {
 }
 
 compose_file(){
-    envsubst < ".stubs/compose/_base.stub" > "$1/.devcontainer/docker-compose.yml"
+    envsubst < ".stubs/compose/_base.stub" > "$1/docker-compose.yml"
 }
 
 compose_services() {
@@ -98,7 +104,9 @@ if [[ "$CHOICE" == "Dockerfile" ]]; then
 #If user chooses a docker-compose: create docker-compose.yml file, and ask for services to include
 elif [[ "$CHOICE" == "Docker-Compose" ]]; then
     echo "So Docker Compose it is"
-    touch "$DIR/docker-compose.yml" #WIP: ask for services to include in the yml file
+    touch "$DIR/docker-compose.yml" 
+    compose_services   
+    compose_file "$DIR"
 #If user chooses to create a devcontainer project, create the hidden devcontainer directory, 
 # and ask what kind of project. [standalone, compose] to add the correct json config
 elif [[ "$CHOICE" == "Devcontainer" ]]; then
@@ -106,8 +114,8 @@ elif [[ "$CHOICE" == "Devcontainer" ]]; then
     mkdir "$DIR/.devcontainer"
     CONTAINERCHOICE=$(gum choose "compose" "standalone")
     if [[ "$CONTAINERCHOICE" == "compose" ]]; then
-        touch "$DIR/.devcontainer/Dockerfile" #for now empty: wip
-        touch "$DIR/.devcontainer/docker-compose.yml" #for now empty: wip
+        touch "$DIR/.devcontainer/Dockerfile" #for now empty
+        touch "$DIR/.devcontainer/docker-compose.yml" #for now empty
         touch "$DIR/.devcontainer/devcontainer.json"
         envsubst < ".stubs/devcontainer/_compose.stub" > "$DIR/.devcontainer/devcontainer.json"
         #envsubst '${localWorkspaceFolderBasename}' < ".stubs/devcontainer/_compose.stub" > "$DIR/.devcontainer/devcontainer.json"
@@ -118,7 +126,7 @@ elif [[ "$CHOICE" == "Devcontainer" ]]; then
 
         compose_services
         
-        compose_file "$DIR"
+        compose_file "$DIR/.devcontainer"
 
         
 
@@ -130,6 +138,6 @@ elif [[ "$CHOICE" == "Devcontainer" ]]; then
     fi
 fi
 
-sleep 1; clear;
+#sleep 1; clear;
 
-echo "Done!"
+#echo "Done!"
