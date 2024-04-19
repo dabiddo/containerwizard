6a1b62197494890c1234e6f8e9c2745dd8ca9cd8
Dabiddo <dabiddo@gmail.com>
2024-04-18 18:43:36 -0700

    added laravel docker-compose & devcontainer files

--- vwizard.sh
+++ vwizard.sh
@@ -3,6 +3,7 @@
 # Function to create a new Laravel project using Docker
 create_laravel_project() {
     read -p "Enter project name: " project_name
+    #docker run --rm -v $(pwd):/app composer create-project --prefer-dist laravel/laravel "$project_name" && sh -c "chown -R $(id -u):$(id -g) $project_name"
     docker run --rm -v "$(pwd)":/app composer create-project --prefer-dist laravel/laravel "$project_name"
     sudo chown -R "$USER":"$USER" "$project_name"
     echo "Ownership changed to $USER:$USER for '$project_name'."
@@ -61,9 +62,9 @@ create_compose_dev_container(){
     touch "$devcontainer_dir/devcontainer.json"
     echo "devcontainer.json created successfully inside '$devcontainer_dir'."
     
-    envsubst < ".stubs/devcontainer/_compose.stub" > "$DIR/.devcontainer/devcontainer.json"
+    envsubst < ".stubs/devcontainer/_laravel.stub" > "$DIR/.devcontainer/devcontainer.json"
 
-    unset DIR
+    
 
     sed -i 's/LOCAL_WORKSPACE_FOLDER_BASENAME/${localWorkspaceFolderBasename}/g' "$devcontainer_dir/devcontainer.json"
 
@@ -71,16 +72,24 @@ create_compose_dev_container(){
     touch "$devcontainer_dir/Dockerfile" #for now empty
     touch "$devcontainer_dir/docker-compose.yml"
 
-    compose_file "$devcontainer_dir"
+    laravel_compose_file "$devcontainer_dir"
+
+    unset DIR
 }
 
+# Replace the docker-compose.yml file with a default
 compose_file(){
     envsubst < ".stubs/compose/_base.stub" > "$1/docker-compose.yml"
 }
 
+# Replace the docker-compose.yml file with the one for laravel
+laravel_compose_file(){
+    envsubst < ".stubs/compose/_laravel.stub" > "$1/docker-compose.yml"
+}
+
 
 # Main Menu
-gum style --border normal --margin "1" --padding "1 2" --border-foreground 12 "Hello, there! Welcome to $(gum style --foreground 12 'DockerWizard')."
+gum style --border normal --margin "1" --padding "1 2" --border-foreground 12 "Hello, there! Welcome to $(gum style --foreground 12 'DevContainerWizard')."
 
 echo "What do you want to do?"
 CHOICE=$(gum choose --item.foreground 250 "Create a new Laravel project" "Create a new Nuxt.Js project" "Create a new NestJs project" "Create a new Refine.dev project" "Quit")
